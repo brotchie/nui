06afc20d452ce2c841650798a3c15ee803165251
James Brotchie <brotchie@gmail.com>
2013-01-04 15:13:11 +1000

    Added NUITests unit tests and Podfile to link Parcoa.

---
+++ NUITests/NUITests/NUITests.h
@@ -0,0 +1,13 @@
+//
+//  NUITests.h
+//  NUITests
+//
+//  Created by James Brotchie on 4/01/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NUITests : SenTestCase
+
+@end
---
+++ NUITests/NUITests/NUITests.m
@@ -0,0 +1,32 @@
+//
+//  NUITests.m
+//  NUITests
+//
+//  Created by James Brotchie on 4/01/13.
+//
+//
+
+#import "NUITests.h"
+
+@implementation NUITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NUITests");
+}
+
+@end
